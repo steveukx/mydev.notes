90cbacf8d717ad79d84aa8e0c48174c8f06641ba
Steve King <steve@mydev.co>
2015-05-09 06:52:51 +0100

    Explicitly set cache lifespan of the ping route to be zero ttl

--- src/server/routes/statics.js
+++ src/server/routes/statics.js
@@ -1,12 +1,14 @@
 
 var app = module.exports = require('express')();
 
+app.use(require('cacheable-middleware')());
+
 app.get('/', function (req, res) {
     res.render('index');
 });
 
 app.get('/ping', function (req, res) {
-    res.send({
+    res.cacheFor(0).send({
         loggedIn: !!req.user
     });
 });
