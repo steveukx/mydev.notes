8431c4ab4699e92171b25c9de941cee8b3143afb
Steve King <steve.king@excelian.com>
2018-03-26 13:28:44 +0100

    Update the templates - grunt upgrade removed `version` as a default declaration

--- GruntFile.js
+++ GruntFile.js
@@ -35,9 +35,9 @@ module.exports = function (grunt) {
       release: {
          options: {
             file: 'package.json',
-            tagName: '<%= version %>', //default: '<%= version %>'
-            commitMessage: 'Release <%= version %>', //default: 'release <%= version %>'
-            tagMessage: 'Tag version <%= version %>' //default: 'Version <%= version %>'
+            tagName: '<%= pkg.version %>', //default: '<%= version %>'
+            commitMessage: 'Release <%= pkg.version %>', //default: 'release <%= version %>'
+            tagMessage: 'Tag version <%= pkg.version %>' //default: 'Version <%= version %>'
          }
       },
 
